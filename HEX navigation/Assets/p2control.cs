4cf58e719c98dac3a594b6a31d0f0e1756a81ac1
miha4406 <65572229+miha4406@users.noreply.github.com>
2021-05-22 17:54:36 +0900

    5/22

---
+++ HEX navigation/Assets/p2control.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class p2control : MonoBehaviour
+{
+    RaycastHit hit;
+    Vector3 clHex;
+
+    [SerializeField] GameObject pFinder;
+    [SerializeField] GameObject pathPref;
+    public int plDist = 2;
+    public GameObject[] hexes = new GameObject[20];
+    public GameObject[] nearHex = new GameObject[7];
+    public Vector3[] path = new Vector3[6];
+
+    Vector3 pfCor; //pathfinder Y-height correction
+    int pfStepNo = 0;
+
+    void Awake()
+    {  
+        clHex = pFinder.transform.position;
+
+        pfCor = pFinder.transform.position + Vector3.down;
+        
+    }
+    void Start()
+    {
+        
+
+    }
+
+    void OnEnable()
+    {
+        foreach (GameObject x in hexes)
+        {
+            if (x != null)
+            {
+                if (Vector3.Distance(x.transform.position, transform.position) < plDist+0.5f)
+                {
+                    x.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                    x.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.yellow);
+                }
+                else
+                {
+                    x.GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", Color.black);
+                }
+            }
+        }
+
+        path = new Vector3[6] { Vector3.zero, Vector3.down, Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+        pFinder.transform.position = transform.position + Vector3.up;
+        clHex = transform.position;
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+
+    void Update()
+    {
+        pfCor = pFinder.transform.position + Vector3.down;
+
+        Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);  //click detect
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (Physics.Raycast(mRay, out hit))
+            {
+                clHex = hit.collider.transform.position;
+
+                path = new Vector3[6] { Vector3.zero, Vector3.down, Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+                pfCor = transform.position;
+
+                InvokeRepeating("pfRoutine", 0f, 0.1f);
+            }
+        }
+        if (clHex == pfCor)
+        {
+            CancelInvoke("pfRoutine"); pfStepNo = 0;
+
+            for (int i = 1; i <= plDist; i++) //path highlights
+            {
+                Instantiate(pathPref, path[i], transform.rotation);
+            }
+        }
+
+
+    }
+
+    void nearHexSearch() // +correction to hex pos (x,0,z)!!!
+    {
+        int j = 1;
+        for (int i = 1; i <= hexes.Length - 1; i++)
+        {
+            if (Vector3.Distance(hexes[i].transform.position, pfCor) < 1.15f) //hex size!
+            {
+                if (hexes[i].transform.position != pfCor)
+                {
+                    nearHex[j] = hexes[i];
+                    j++;
+                }
+            }
+        }
+
+    }
+
+    void pathFind()
+    {
+        int s = 1;
+        path[pfStepNo] = nearHex[1].transform.position;
+        float nearest = Vector3.Distance(nearHex[1].transform.position, clHex);
+
+        for (int i = 1; i <= nearHex.Length - 1; i++)
+        {
+            if (Vector3.Distance(nearHex[i].transform.position, clHex) < nearest
+            && Vector3.Distance(nearHex[i].transform.position, pfCor) < 1.5f) //hex size!
+            {
+                path[pfStepNo] = nearHex[i].transform.position;
+                nearest = Vector3.Distance(nearHex[i].transform.position, clHex);
+                s = i; //nearest hex number in nearHex[]
+            }
+        }
+
+        pFinder.transform.position = nearHex[s].transform.position + Vector3.up;
+    }
+
+    void pfRoutine()
+    {
+        pfStepNo++;
+        nearHexSearch();
+        pathFind();       
+    }
+
+
+}
+
---
+++ HEX navigation/Assets/turnEnd.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class turnEnd : MonoBehaviour
+{    
+    [SerializeField] float plMovSp = 1f;
+    [SerializeField] GameObject pl1;
+    [SerializeField] GameObject pl2;
+    [SerializeField] GameObject pl3;
+    public int pl1dist = 2; 
+    public int pl2dist = 2;
+    public int pl3dist = 2; //?
+
+    public Vector3[] p1Path = new Vector3[6];    
+    int mov1StepNo = 1;
+    public int lastMov1 = 0;
+    bool bMove1 = false;
+
+    public Vector3[] p2Path = new Vector3[6];
+    int mov2StepNo = 1;
+    public int lastMov2 = 0;
+    bool bMove2 = false;
+
+    public Vector3[] p3Path = new Vector3[6];
+    int mov3StepNo = 1;
+    public int lastMov3 = 0;
+    bool bMove3 = false;
+
+    bool bWait = false;
+
+    void Update()
+    {
+        if (bWait) { print("WAIT"); }
+
+        if (!bWait)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                pl1.GetComponent<plControl>().enabled = true;
+                pl2.GetComponent<p2control>().enabled = false;
+                pl3.GetComponent<p3control>().enabled = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                pl1.GetComponent<plControl>().enabled = false;
+                pl2.GetComponent<p2control>().enabled = true;
+                pl3.GetComponent<p3control>().enabled = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                pl1.GetComponent<plControl>().enabled = false;
+                pl2.GetComponent<p2control>().enabled = false;
+                pl3.GetComponent<p3control>().enabled = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            p1Path = pl1.GetComponent<plControl>().path;
+            p1Path[0] = pl1.transform.position; //can return to start pos
+            bMove1 = true;
+
+            p2Path = pl2.GetComponent<p2control>().path;
+            p2Path[0] = pl2.transform.position;
+            bMove2 = true;
+
+            p3Path = pl3.GetComponent<p3control>().path;
+            p3Path[0] = pl3.transform.position;
+            bMove3 = true;
+
+            pl1.GetComponent<plControl>().enabled = false;
+            pl2.GetComponent<p2control>().enabled = false;
+            pl3.GetComponent<p3control>().enabled = false;
+        }
+
+        if (bMove1) { bWait = true; pl1move(mov1StepNo); }
+        if (bMove2) { bWait = true; pl2move(mov2StepNo); }
+        if (bMove3) { bWait = true; pl3move(mov3StepNo); }
+
+
+     /////////////////////collisions//////////////////////////
+        if (!bMove1 && !bMove2 && !bMove3) { 
+
+             if (pl1.transform.position == pl2.transform.position) { StartCoroutine(plCollision(pl1, pl2, pl3)); }
+             if (pl1.transform.position == pl3.transform.position) { StartCoroutine(plCollision(pl1, pl3, pl2)); }
+             if (pl2.transform.position == pl3.transform.position) { StartCoroutine(plCollision(pl2, pl3, pl1)); }             
+        }
+        if (pl1.GetComponent<stats>().hitCount!=0)
+        {
+            print("pl1 hits"); 
+            if (lastMov1-pl1.GetComponent<stats>().hitCount >= 0) { pl1.transform.position = Vector3.MoveTowards(pl1.transform.position, p1Path[lastMov1-pl1.GetComponent<stats>().hitCount], plMovSp * 1.05f * Time.deltaTime); }       
+        }
+        if (pl2.GetComponent<stats>().hitCount!=0)
+        {
+            print("pl2 hits"); 
+            if (lastMov2-pl2.GetComponent<stats>().hitCount >= 0) { pl2.transform.position = Vector3.MoveTowards(pl2.transform.position, p2Path[lastMov2-pl2.GetComponent<stats>().hitCount], plMovSp * Time.deltaTime); }
+        }
+        if (pl3.GetComponent<stats>().hitCount != 0)
+        {
+            print("pl3 hits"); 
+            if (lastMov3-pl3.GetComponent<stats>().hitCount >= 0) { pl3.transform.position = Vector3.MoveTowards(pl3.transform.position, p3Path[lastMov3-pl3.GetComponent<stats>().hitCount], plMovSp * 0.95f * Time.deltaTime); }
+        }
+    }
+
+    IEnumerator plCollision(GameObject plA, GameObject plB, GameObject plC)
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        if (plA.transform.position == plB.transform.position)
+        {
+            //yield return new WaitForSeconds(0.5f);
+            plA.GetComponent<stats>().hitCount++; 
+            plB.GetComponent<stats>().hitCount++;            
+            if (plB.transform.position == plC.transform.position)
+            {
+                plC.GetComponent<stats>().hitCount++;
+            }
+
+            bWait = true;
+
+            StopAllCoroutines();       
+            //StopCoroutine(hitCountClear(plA, plB));
+            StartCoroutine( hitCountClear(plA, plB, plC) );
+        }
+      
+    }  
+    IEnumerator hitCountClear(GameObject plA, GameObject plB, GameObject plC)
+    {        
+        yield return new WaitForSeconds(plMovSp * 1.5f); //malo?
+        plA.GetComponent<stats>().hitCount = 0;
+        plB.GetComponent<stats>().hitCount = 0;
+        plC.GetComponent<stats>().hitCount = 0;
+        
+        bWait = false;
+    }
+
+
+    void pl1move(int stepNo) 
+    {
+        if (p1Path[stepNo] != Vector3.down && (stepNo <= pl1dist))
+        {
+            pl1.transform.position = Vector3.MoveTowards(pl1.transform.position, p1Path[stepNo], plMovSp*1.05f *Time.deltaTime);
+
+            pl1.transform.LookAt(p1Path[stepNo]);
+
+            if (pl1.transform.position == p1Path[stepNo])
+            {
+                mov1StepNo++;
+            }
+        }
+        else { bMove1 = false; lastMov1 = mov1StepNo-1;  mov1StepNo = 1; bWait = false; }       
+    }
+
+    void pl2move(int stepNo)
+    {
+        if (p2Path[stepNo] != Vector3.down && (stepNo <= pl2dist))
+        {
+            pl2.transform.position = Vector3.MoveTowards(pl2.transform.position, p2Path[stepNo], plMovSp*1.00f *Time.deltaTime);
+
+            pl2.transform.LookAt(p2Path[stepNo]);
+
+            if (pl2.transform.position == p2Path[stepNo])
+            {
+                mov2StepNo++;
+            }
+        }
+        else { bMove2 = false; lastMov2 = mov2StepNo-1; mov2StepNo = 1; bWait = false; }
+    }
+    void pl3move(int stepNo)
+    {
+        if (p3Path[stepNo] != Vector3.down && (stepNo <= pl3dist))
+        {
+            pl3.transform.position = Vector3.MoveTowards(pl3.transform.position, p3Path[stepNo], plMovSp * Time.deltaTime);
+
+            pl3.transform.LookAt(p3Path[stepNo]);
+
+            if (pl3.transform.position == p3Path[stepNo])
+            {
+                mov3StepNo++;
+            }
+        }
+        else { bMove3 = false; lastMov3 = mov3StepNo-1; mov3StepNo = 1; bWait = false; } 
+    }
+
+
+
+
+
+  
+}
