121086bf107fdb06c84110a154a4ed8f1992702d
miha4406 <65572229+miha4406@users.noreply.github.com>
2021-05-31 02:56:58 +0900

    new gold behavior
    Fixed gold behavior. All done.
    If project misses some materials or scripts, it's GitHub to blame(

--- HEX navigation/Assets/scripts/goldControl.cs
+++ HEX navigation/Assets/scripts/goldControl.cs
@@ -4,11 +4,14 @@ using System.Linq;
 
 public class goldControl : MonoBehaviour
 {
-    [SerializeField] GameObject gBar;
+    [SerializeField] GameObject gBar1;
+    [SerializeField] GameObject gBar2;
+    [SerializeField] GameObject gBar3;
 
     [SerializeField] GameObject pl1;
     [SerializeField] GameObject pl2;
     [SerializeField] GameObject pl3;
+   
 
     public GameObject[] hexes = new GameObject[20];
     public GameObject[] itemHexes = new GameObject[3]; // item hexes - h1, h12, h17
@@ -18,7 +21,7 @@ public class goldControl : MonoBehaviour
 
     void Awake()
     {
-        gBar.transform.position = hexes[10].transform.position;       
+        gBar1.transform.position = hexes[10].transform.position;       
     }  
 
     
@@ -27,31 +30,76 @@ public class goldControl : MonoBehaviour
         turnNo = gameObject.GetComponent<turnEnd>().turnNo;
         print("Turn "+turnNo);
 
-        if (movEnd && gBar.active)  //gold pick up
-        {
-            if (gBar.transform.position==pl1.transform.position) { pl1.GetComponent<stats>().gold++; gBar.SetActive(false); }
-            if (gBar.transform.position==pl2.transform.position) { pl2.GetComponent<stats>().gold++; gBar.SetActive(false); }
-            if (gBar.transform.position==pl3.transform.position) { pl3.GetComponent<stats>().gold++; gBar.SetActive(false); }
+        if (turnNo>0) { gHex1(); }
+        if (turnNo>5) { gHex2(); }
+        if (turnNo>10){ gHex3(); }
 
-            movEnd = false;
-        }
-        
-
-        if ((turnNo==6 || turnNo==11) && gBar.active == false) //new gold add
-        {
-            print("add gold");
-
-            gBar.SetActive(true);
-            gBar.transform.position = hexes.Where(el => el != null).Except(itemHexes).OrderBy(n => Random.value).FirstOrDefault().transform.position;
-
-        }       
+        movEnd = false;
 
     }
+
+    void gHex1()
+    {
+        if (gBar1.active == false) //new gBar1 add
+        {
+            gBar1.transform.position = hexes.Where(hex => hex != null && (hex.transform.position != gBar1.transform.position
+            && hex.transform.position != gBar2.transform.position && hex.transform.position != gBar3.transform.position))
+                .Except(itemHexes).OrderBy(n => Random.value).FirstOrDefault().transform.position;
+            gBar1.SetActive(true);
+        }
+
+        if (movEnd && gBar1.active)  //gold pick up
+        {
+            if (gBar1.transform.position == pl1.transform.position) { pl1.GetComponent<stats>().gold++; gBar1.SetActive(false); }
+            if (gBar1.transform.position == pl2.transform.position) { pl2.GetComponent<stats>().gold++; gBar1.SetActive(false); }
+            if (gBar1.transform.position == pl3.transform.position) { pl3.GetComponent<stats>().gold++; gBar1.SetActive(false); }           
+        }
+     
+    }
+    void gHex2()
+    {
+        if (gBar2.active == false) //new gBar2 add
+        {
+            gBar2.transform.position = hexes.Where(hex => hex != null && (hex.transform.position != gBar1.transform.position
+            && hex.transform.position != gBar2.transform.position && hex.transform.position != gBar3.transform.position))
+                .Except(itemHexes).OrderBy(n => Random.value).FirstOrDefault().transform.position;
+            gBar2.SetActive(true);
+        }
+        
+        if (movEnd && gBar2.active)
+        {
+            if (gBar2.transform.position == pl1.transform.position) { pl1.GetComponent<stats>().gold++; gBar2.SetActive(false); }
+            if (gBar2.transform.position == pl2.transform.position) { pl2.GetComponent<stats>().gold++; gBar2.SetActive(false); }
+            if (gBar2.transform.position == pl3.transform.position) { pl3.GetComponent<stats>().gold++; gBar2.SetActive(false); }            
+        }
+    }
+    void gHex3()
+    {
+        if (gBar3.active == false) //new gBar2 add
+        {
+            gBar3.transform.position = hexes.Where(hex => hex != null && (hex.transform.position != gBar1.transform.position
+            && hex.transform.position != gBar2.transform.position && hex.transform.position != gBar3.transform.position))
+                .Except(itemHexes).OrderBy(n => Random.value).FirstOrDefault().transform.position;
+            gBar3.SetActive(true);
+        }
+
+        if (movEnd && gBar3.active)
+        {
+            if (gBar3.transform.position == pl1.transform.position) { pl1.GetComponent<stats>().gold++; gBar3.SetActive(false); }
+            if (gBar3.transform.position == pl2.transform.position) { pl2.GetComponent<stats>().gold++; gBar3.SetActive(false); }
+            if (gBar3.transform.position == pl3.transform.position) { pl3.GetComponent<stats>().gold++; gBar3.SetActive(false); }
+        }
+    }
+
     public void goldSwap(GameObject plA, GameObject plB, GameObject plC)  //gold exchange, used in plCollision (turnEnd.cs)
     {
         if (plA.transform.position==plB.transform.position && plB.transform.position==plC.transform.position)
         {
-            //do nothing?
+            int avg = (pl1.GetComponent<stats>().gold + pl2.GetComponent<stats>().gold + pl3.GetComponent<stats>().gold) / 3;
+            pl1.GetComponent<stats>().gold = avg;
+            pl2.GetComponent<stats>().gold = avg;
+            pl3.GetComponent<stats>().gold = avg;
+           
         }
         else if (plA.transform.position == plB.transform.position)  //will swap EVERY time on collision
         {
